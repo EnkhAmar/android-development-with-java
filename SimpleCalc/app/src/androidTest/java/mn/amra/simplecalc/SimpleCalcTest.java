869ba5de5760164beb3d4a1cdc56d652086f9d18
enkhamar1220@gmail.com <enkhamar1220@gmail.com>
2022-04-16 17:50:09 +0800

    SimpleCalc

--- SimpleCalc/app/src/androidTest/java/mn/amra/simplecalc/SimpleCalcTest.java
+++ SimpleCalc/app/src/androidTest/java/mn/amra/simplecalc/SimpleCalcTest.java
@@ -19,7 +19,7 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 @RunWith(AndroidJUnit4.class)
-public class ExampleInstrumentedTest {
+public class SimpleCalcTest {
     private Calculator mCalculator = new Calculator();
 
     @Test
@@ -30,14 +30,44 @@ public class ExampleInstrumentedTest {
     }
 
     @Test
+    public void addTwoNumbers() {
+        double resultAdd = mCalculator.add(1d, 1d);
+        assertThat(resultAdd, is(equalTo(2d)));
+    }
+
+    @Test
     public void addTwoNumbersNegative() {
         double resultAdd = mCalculator.add(-1d, 2d);
         assertThat(resultAdd, is(equalTo(1d)));
     }
-
     @Test
     public void addTwoNumbersFloats() {
         double resultAdd = mCalculator.add(1.111f, 1.111d);
         assertThat(resultAdd, is(closeTo(2.222, 0.01)));
+    }
+    @Test
+    public void subTwoNumbers() {
+        double resultSub = mCalculator.sub(1d, 1d);
+        assertThat(resultSub, is(equalTo(0d)));
+    }
+    @Test
+    public void subWorksWithNegativeResult() {
+        double resultSub = mCalculator.sub(1d, 17d);
+        assertThat(resultSub, is(equalTo(-16d)));
+    }
+    @Test
+    public void mulTwoNumbers() {
+        double resultMul = mCalculator.mul(32d, 2d);
+        assertThat(resultMul, is(equalTo(64d)));
+    }
+    @Test
+    public void divTwoNumbers() {
+        double resultDiv = mCalculator.div(32d,2d);
+        assertThat(resultDiv, is(equalTo(16d)));
+    }
+    @Test
+    public void divTwoNumbersZero() {
+        double resultDiv = mCalculator.div(32d,0);
+        assertThat(resultDiv, is(equalTo(Double.POSITIVE_INFINITY)));
     }
 }
