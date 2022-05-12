afc9bfde83b247fc0644e0affff356375423c78f
EnkhAmar <enkhamar1220@gmail.com>
2022-05-12 13:06:19 +0800

    lab4

---
+++ lab4/app/src/main/java/mn/amra/lab4/AddWordActivity.java
@@ -0,0 +1,14 @@
+package mn.amra.lab4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddWordActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_word);
+    }
+}
--- lab4/app/src/main/java/mn/amra/lab4/MainActivity.java
+++ lab4/app/src/main/java/mn/amra/lab4/MainActivity.java
@@ -2,13 +2,46 @@ package mn.amra.lab4;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    private int mode;
+    private TextView mMongolianText, mForeignText;
+    private int currentIndex = 0;
+    public SharedPreferences sp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+//        getMode();
+//        mMongolianText = findViewById(R.id.mongolian);
+//        mForeignText = findViewById(R.id.foreign);
     }
+
+
+//    public void handleButtonClick(View view) {
+//        Intent intent = new Intent();
+////        intent.setClass(getBaseContext());
+//        switch (view.getId()) {
+//            case R.id.previousButton:
+//                return;
+//            case R.id.nextButton:
+//                return;
+//            case R.id.deleteButton:
+//                return;
+//            case R.id.editButton:
+//                return;
+//            case R.id.addButton:
+//                return;
+//            default:
+//                return;
+//        }
+//    }
 }
